af09c9da57b75d4804f47d95ea6b673828bcbbd1
Pham Huu Doi <doipham1812@gmail.con>
2025-10-31 03:03:45 +0700

    deploy test

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,14 @@ import cors from "cors";
 import jwt from "jsonwebtoken";
 import bcrypt from "bcrypt";
 import dotenv from "dotenv";
+import path from "path";
+
 
 dotenv.config();
 const app = express();
 app.use(cors());
 app.use(express.json());
+const __dirname = path.resolve();
 
 const users = [
   { id: 1, username: "admin", password: await bcrypt.hash("123456", 10) },
@@ -28,6 +31,6 @@ app.post("/login", async (req, res) => {
 
   res.json({ token });
 });
-
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Backend running on port ${PORT}`));
